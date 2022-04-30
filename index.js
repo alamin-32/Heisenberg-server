5e5b783346fe454e1833e6419f8094b34e9234a8
alamin-32 <alaminhussain32@gmail.com>
2022-05-01 02:58:10 +0600

    delete added

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query)
+            res.send(result)
+        })
+
     }
     finally {
         // await client.close();

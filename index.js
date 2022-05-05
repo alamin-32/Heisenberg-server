812410131a5c7b24c8d43da5d6b3e5cfe0a0519e
alamin-32 <alaminhussain32@gmail.com>
2022-05-05 15:59:58 +0600

    fianl push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
-
+console.log(app);
 
 app.use(cors())
 app.use(express.json())
